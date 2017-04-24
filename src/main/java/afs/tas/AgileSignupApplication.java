26fc0062efad80ff0d7a7e3aaab1255560c8812d
bepastrana <blake.pastrana@accenturefederal.com>
2017-04-24 14:56:55 -0400

    Initial Project

---
+++ src/main/java/afs/tas/AgileSignupApplication.java
@@ -0,0 +1,12 @@
+package afs.tas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileSignupApplication.class, args);
+	}
+}
